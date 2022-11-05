9690eb1f5cde1f6c82c49c808c332ba410aa9d74
fatemeRasolzade <fatemerasolzade.1424@gmail.com>
2022-11-05 21:45:04 +0330

    create project structure

---
+++ containers/home/HomePage.js
@@ -0,0 +1,13 @@
+const HomePage = () => {
+  return (
+    <main className="banner ">
+      <div className="absolute top-36 right-[24rem] text-white font-ReenieBeanie">
+        <p className="text-8xl font-bold">Typiano</p>
+        <p className="text-3xl pl-10">Enjoy the sound of your keyboard</p>
+        <p className="text-3xl pl-10">without looking at it</p>
+      </div>
+    </main>
+  );
+};
+
+export default HomePage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./containers/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        ReenieBeanie: ["ReenieBeanie"],
+      },
+    },
   },
   plugins: [],
 };
