58b5fb859cc5cc215a8b2e4f0108785a8c2c64d4
fatemeRasolzade <fatemerasolzade.1424@gmail.com>
2023-01-06 20:32:25 +0330

    set status for courses page

--- containers/courses/_components/CoursesElements.js
+++ containers/courses/_components/CoursesElements.js
@@ -1,16 +1,36 @@
 import CheckIcon from "public/icons/CheckIcon";
+import LockIcon from "public/icons/LockIcon";
+import PlayIcon from "public/icons/PlayIcon";
 
-const CoursesElements = () => (
-  <div className="flex-between-center bg-white w-full px-4 rounded-xl">
-    <div className="flex-start-center gap-3">
-      <img src={"/images/courses/coffee.png"} width={70} alt="image" />
-      <div>
-        <p className=" font-semibold">lesson1</p>
-        <p className="text-gray-dark text-[.65rem]">5min . 8 exercises</p>
+const CoursesElements = ({ item, index }) => {
+  //   const ConvertImage = () => {
+  //     if (index % 3 === 0) return;
+  //     if (index % 2 === 0) return;
+  //     if (index % 1 === 0) return;
+  //     if (index % 0 === 0) return;
+  //   };
+  const images = {
+    1: <img src={"/images/courses/Pencils.png"} width={70} alt="image" />,
+    2: <img src={"/images/courses/Pin.png"} width={70} alt="image" />,
+    3: <img src={"/images/courses/coffee.png"} width={70} alt="image" />,
+    4: <img src={"/images/courses/Box.png"} width={70} alt="image" />,
+  };
+  const icons = {
+    1: <CheckIcon />,
+    2: <PlayIcon />,
+    3: <LockIcon />,
+  };
+  return (
+    <div className={`flex-between-center bg-white w-full px-4 rounded-xl ${item.status > 2 ? "grayscale bg-gray-light" : ""} `}>
+      <div className="flex-start-center gap-3">
+        {images[item.id]}
+        <div>
+          <p className=" font-semibold">{item?.name}</p>
+          <p className="text-gray-dark text-[.65rem]">5min . 8 exercises</p>
+        </div>
       </div>
+      {icons[item.status]}
     </div>
-    <CheckIcon />
-  </div>
-);
-
+  );
+};
 export default CoursesElements;
