321e4679bd61e55dbc48e66c540fb2fa8ec0bf34
fatemeRasolzade <fatemerasolzade.1424@gmail.com>
2023-01-26 10:02:46 +0330

    fiexed bug inner topNav

--- components/common/TopNav.js
+++ components/common/TopNav.js
@@ -6,15 +6,12 @@ import UserIcon from "public/icons/UserIcon";
 const TopNav = () => {
   const { pathname } = useRouter();
   const DesktopNavElements = ({ title, path }) => (
-    <Link
-      href={path}
-      className={`px-3 py-1.5 font-semibold rounded-full ${pathname === path ? "text-dark bg-mainBlue" : "text-gray-4 "}`}
-    >
+    <Link href={path} className={`px-3 py-1.5 font-semibold rounded-full ${pathname === path ? "text-dark bg-mainBlue" : "text-gray-4 "}`}>
       {title}
     </Link>
   );
   return (
-    <header className="flex-between-center max-w-[85rem] mx-auto px-2 mt-5 ">
+    <header className="relative flex-between-center max-w-[85rem] mx-auto px-2 mt-5 bg-transparent z-30">
       <h1>Typiano</h1>
       <nav className="flex-between-center gap-5">
         <DesktopNavElements title="Home" path="/" />
