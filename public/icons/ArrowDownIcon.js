e3a8e34ec2936671012130bcd9044cff1ef03fe7
fatemeRasolzade <fatemerasolzade.1424@gmail.com>
2023-01-27 11:48:09 +0330

    fixed consol errors

--- public/icons/ArrowDownIcon.js
+++ public/icons/ArrowDownIcon.js
@@ -1,6 +1,6 @@
 const ArrowDownIcon = () => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M8.47021 10.74L12.0002 14.26L15.5302 10.74" stroke="#1E2229" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+    <path d="M8.47021 10.74L12.0002 14.26L15.5302 10.74" stroke="#1E2229" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
   </svg>
 );
 
