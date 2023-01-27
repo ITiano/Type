b8f050f6a327aac4cd2ebdfaedb2df64a7290578
fatemeRasolzade <fatemerasolzade.1424@gmail.com>
2023-01-27 16:40:32 +0330

    add option to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./containers/**/*.{js,ts,jsx,tsx}"],
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./containers/**/*.{js,ts,jsx,tsx}", "./public/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
