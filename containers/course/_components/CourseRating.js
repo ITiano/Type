c90ce166fbd2aea38d22431ae65528533b2520f5
fatemeRasolzade <fatemerasolzade.1424@gmail.com>
2023-02-18 23:18:18 +0330

    developed course rating

--- containers/course/_components/CourseRating.js
+++ containers/course/_components/CourseRating.js
@@ -10,6 +10,27 @@ const CourseRating = () => {
         {CoursesImages(140)[1]}
         <p className=" font-semibold pb-1.5 mt-1 text-2xl">Untitled Lesson 2</p>
         <Stars value={4} />
+        <div className="flex-between-center mt-5 gap-10">
+          <div className="text-center">
+            <p>Accuracy</p>
+            <p className="font-semibold mt-2">58</p>
+          </div>
+          <div className=" w-px h-5 bg-gray-3 bg-opacity-30"></div>
+          <div className="text-center">
+            <p>Accuracy</p>
+            <p className="font-semibold mt-2">58</p>
+          </div>
+          <div className=" w-px h-5 bg-gray-3 bg-opacity-30"></div>
+          <div className="text-center">
+            <p>Speed</p>
+            <p className="font-semibold mt-2">25</p>
+          </div>
+          <div className=" w-px h-5 bg-gray-3 bg-opacity-30"></div>
+          <div className="text-center">
+            <p>Time</p>
+            <p className="font-semibold mt-2">02:48</p>
+          </div>
+        </div>
       </div>
       <div className="bg-white py-10 px-16 w-full flex-between-center">
         <CustomBtn text="previous lesson" arrowStartBtn onClick={() => setStep(2)} />
