31e1ad04878b332f2ff739b76825426bfc7fcade
AmirShafikhani <amirshafikhani.official@gmail.com>
2023-03-10 16:33:06 +0330

    course history modal added

--- components/courses/CourseHistory.js
+++ components/courses/CourseHistory.js
@@ -7,10 +7,10 @@ import { CoursesIcons } from "helper/Methods";
 // Components
 import Stars from "components/common/Stars";
 
-const CourseHistoryElement = ({ course, kind }) => {
+const CourseHistory = ({ course, kind }) => {
   return (
     <div className={`${kind === 1 ? "hidden md:flex-start-center gap-8" : "centering flex-col gap-3"}`}>
-      <div className="centering gap-4">
+      <div className="centering gap-4 ">
         {course.status === 1 && <ElementBox title="Starts" value={course.rating} kind={kind} star />}
         {kind === 2 && (
           <>
@@ -23,7 +23,7 @@ const CourseHistoryElement = ({ course, kind }) => {
         )}
       </div>
       {course.status === 1 && (
-        <div className={`${kind === 1 ? "hidden md:flex-start-center gap-8" : "grid grid-cols-4 gap-3"}`}>
+        <div className={`${kind === 1 ? "hidden md:flex-start-center gap-8" : "grid grid-cols-4 gap-3 flex-1"}`}>
           <ElementBox title="Score" value={course.score} kind={kind} />
           <ElementBox title="Speed" value={course.speed} kind={kind} />
           <ElementBox title="Accuracy" value={course.accuracy} kind={kind} />
@@ -34,7 +34,7 @@ const CourseHistoryElement = ({ course, kind }) => {
   );
 };
 
-export default CourseHistoryElement;
+export default CourseHistory;
 
 const ElementBox = ({ title, value, star, kind }) => (
   <div className={`${kind === 1 ? "hidden md:flex-start-start flex-col" : "centering flex-col"} gap-2`}>
--- components/courses/HistoryModal.js
+++ components/courses/HistoryModal.js
@@ -1,10 +1,21 @@
-import ModalContainer from "components/utils/ModalContainer";
 import React from "react";
+
+// Components
+import CourseDetail from "./CourseDetail";
+import CourseHistory from "./CourseHistory";
+import ModalContainer from "components/utils/ModalContainer";
 
 const HistoryModal = ({ open, setOpen }) => {
   return (
-    <ModalContainer open={open} setOpen={setOpen} width="600px" clear >
-      salam
+    <ModalContainer open={open} setOpen={setOpen} width="700px" clear>
+      <div className="flex-start-center [&>*]:flex-row mb-4">
+        <CourseDetail course={open} kind={2} />
+      </div>
+      <div className="flex flex-col gap-4 md:[&>*]:flex-row [&>*]:shadow [&>*]:rounded [&>*]:p-4">
+        {new Array(10).fill("").map((item, index) => (
+          <CourseHistory key={index} course={open} kind={2} />
+        ))}
+      </div>
     </ModalContainer>
   );
 };
