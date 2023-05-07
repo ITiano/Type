c38112bc915770ff205b6c59a7b9d974317b706d
fatemeRasolzade <fatemerasolzade.1424@gmail.com>
2023-05-08 01:49:57 +0430

    create spinner component

--- components/utils/CustomBtn.js
+++ components/utils/CustomBtn.js
@@ -1,27 +1,13 @@
 import ArrowLeftIcon from "public/icons/ArrowLeftIcon";
 import ArrowRightIcon from "public/icons/ArrowRightIcon";
+import Spinner from "./Spinner";
 
-const CustomBtn = ({
-  text,
-  endIcon,
-  onClick,
-  loading,
-  disabled,
-  className,
-  startIcon,
-  arrowEndBtn,
-  arrowStartBtn,
-  type = "button",
-}) => {
+const CustomBtn = ({ text, endIcon, onClick, loading, disabled, className, startIcon, arrowEndBtn, arrowStartBtn, type = "button" }) => {
   return (
-    <button
-      type={type}
-      onClick={onClick}
-      disabled={loading || disabled}
-      className={`btn centering gap-2 ${className} ${disabled && "opacity-70"}`}
-    >
+    <button type={type} onClick={onClick} disabled={loading || disabled} className={`btn centering gap-2 ${className} ${disabled && "opacity-70"}`}>
       {startIcon}
       {arrowStartBtn && <ArrowLeftIcon />}
+      {loading && <Spinner />}
       {text}
       {arrowEndBtn && <ArrowRightIcon />}
       {endIcon}
