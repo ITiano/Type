bc9c6cd08bdfe00383fd72198c8c464e042bd7d5
Soheil <jafarnejad.official@gmail.com>
2023-05-13 21:51:37 +0330

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,12 @@
 // Global css
 import "../styles/App.css";
-
 // SEO
 import SEO from "next-seo.config";
 import { DefaultSeo } from "next-seo";
-
 // toolkit
 import { Provider } from "react-redux";
 import { store } from "redux/store";
-
 import { Toaster } from "react-hot-toast";
-
 const MyApp = ({ Component, pageProps }) => {
   return (
     <Provider store={store}>
