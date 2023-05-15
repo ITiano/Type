a11348ad37da524dad78f976941c80e80d2bc191
Soheil <jafarnejad.official@gmail.com>
2023-05-15 21:01:51 +0330

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 // Global css
 import "../styles/App.css";
+
 // SEO
 import SEO from "next-seo.config";
 import { DefaultSeo } from "next-seo";
 // toolkit
+
 import { Provider } from "react-redux";
 import { store } from "redux/store";
 import { Toaster } from "react-hot-toast";
