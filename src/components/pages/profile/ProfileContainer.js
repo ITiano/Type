74f5f5c3c1240c1785754d461ba7df58ac50ca28
Soheil <jafarnejad.official@gmail.com>
2023-07-28 12:30:20 +0330

    update text navigator.canShare

--- src/components/pages/profile/ProfileContainer.js
+++ src/components/pages/profile/ProfileContainer.js
@@ -27,11 +27,10 @@ const ProfileContainer = ({ data }) => {
         const imageFile = await htmlToImage.toBlob(ref.current);
         const title = "Typiano - Your 10-Finger Typing Maestro.";
         const text =
-          "Step into a world of typing excellence with Typiano! Improve your typing speed, accuracy, and efficiency through engaging lessons and challenging exercises. Whether you're a beginner or a seasoned typist, Typiano will help you become a typing virtuoso. Embrace the joy of typing and unlock your true potential!";
-        const url = "https://typiano.vercel.app/";
+          "Step into a world of typing excellence with Typiano! Improve your typing speed, accuracy, and efficiency through engaging lessons and challenging exercises. Whether you're a beginner or a seasoned typist, Typiano will help you become a typing virtuoso. Embrace the joy of typing and unlock your true potential! Visit Typiano's website: https://typiano.vercel.app/";
         const files = [new File([imageFile], fileName + ".png", { type: "image/png" })];
         if (navigator.canShare && navigator.canShare({ files })) {
-          await navigator.share({ title, text, url, files });
+          await navigator.share({ title, text, files });
           setLoading(false);
         } else {
           const imageDataUrl = await htmlToImage.toPng(ref.current);
