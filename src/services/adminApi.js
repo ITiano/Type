6ef7c53a207e9526c9be9634041a8c98dd3e004c
fatemeRasolzade <fatemerasolzade.1424@gmail.com>
2024-06-23 15:57:20 +0330

    :tada: created-next-app

--- src/services/adminApi.js
+++
@@ -1,9 +0,0 @@
-import clientSupabase from "./supabase-client";
-
-export const getCoursesCount = () => clientSupabase.from("courses").select("", { count: "exact" });
-
-export const addCourse = (data) => clientSupabase.from("courses").insert(data);
-
-export const updateCourse = (data) => clientSupabase.from("courses").update(data).eq("id", data.id).select();
-
-export const deleteCourse = (id) => clientSupabase.from("courses").delete().eq("id", id);
